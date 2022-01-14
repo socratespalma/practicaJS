6f35abef1b093f2452899ae802b5b22c2e80becb
Socrates Palma <dubstepero10@gmail.com>
2022-01-13 23:20:54 -0600

    added some changes in hoaf dir

--- JS/hoaf/index.js
+++ JS/hoaf/index.js
@@ -3,12 +3,31 @@ const getPokemons = async () => {
     "https://pokeapi.co/api/v2/pokemon?limit=50&offset=0"
   );
   let data = await res.json();
-  let newArr = [];
+  let pokemonNames = data.results;
+  console.log(pokemonNames);
 
-  data.results.map((pokemon) => {
-    newArr.push(pokemon.name);
-  });
-  console.log(newArr);
+  // * For loop
+  // for (let i = 0; i < data.results.length; i++) {
+  //   const element = data.results[i];
+  //   console.log(element);
+  // }
+
+  // * For each
+  // data.results.forEach((pokemon) => {
+  //   console.log(pokemon.name);
+  // });
+
+  // * Filter method
+  // const filteredNames = data.results.name.filter((name) => {
+  //   name.startsWith("B");
+  // });
+
+  // let newArr = [];
+
+  // data.results.map((pokemon) => {
+  //   newArr.push(pokemon.name);
+  // });
+  // console.log(newArr);
 };
 
 getPokemons();
